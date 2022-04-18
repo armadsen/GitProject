039f212c55bc33f440880960027a4a164be21dc2
Andrew Madsen <andrew@openreelsoftware.com>
2022-04-18 09:47:09 -0600

    Initial commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Andrew R Madsen on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
